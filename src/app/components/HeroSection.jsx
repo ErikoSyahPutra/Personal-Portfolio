2b74ebb6d8e753f7b31826ab974ee76b12d7d601
Eriko Syah <erikosyahputraf@gmail.com>
2025-01-09 20:05:32 +0700

    first commit

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+          Hello, I'm Eriko Syah
+        </h1>
+        <p className="text-[#ADB7BE] text-lg lg:text-xl">
+          Lorem Ipsum dolor sit amet{" "}
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
