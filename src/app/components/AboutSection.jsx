abdcc9fe12c2f82c90f1c534d91165b5f8ff0ebd
Eriko Syah <erikosyahputraf@gmail.com>
2025-01-15 18:19:57 +0700

    Update: About Section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -9,12 +9,12 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="list-disc pl-2">
-        <li>HTML</li>
-        <li>CSS</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>PostgreSQL</li>
+        <li>Sequelize</li>
         <li>JavaScript</li>
         <li>React</li>
-        <li>NextJS</li>
-        <li>Tailwind</li>
       </ul>
     ),
   },
@@ -23,8 +23,8 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>SMA Negeri 4 Pekalongan</li>
-        <li>Universitas Negeri Semarang</li>
+        <li>Fullstack Academy of Code</li>
+        <li>University of California, Santa Cruz</li>
       </ul>
     ),
   },
@@ -33,9 +33,8 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
+        <li>AWS Cloud Practitioner</li>
+        <li>Google Professional Cloud Developer</li>
       </ul>
     ),
   },
@@ -52,45 +51,44 @@ const AboutSection = () => {
   };
 
   return (
-    <section className="text-white">
+    <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image
-          src="/images/artikel-komputer.jpg"
-          width={500}
-          height={500}
-          alt="About Me Image"
-        />
+        <Image src="/images/about-image.png" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base md:text-lg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            voluptuous voluptas dignissimos aliquam. Quisquam voluptuous
-            voluptas dignissimos aliquam. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Quisquam voluptuous voluptas dignissimos aliquam.
-            Quisquam voluptuous voluptas dignissimos aliquam.
+          <p className="text-base lg:text-lg">
+            I am a full stack web developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
+            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
+            looking to expand my knowledge and skill set. I am a team player and
+            I am excited to work with others to create amazing applications.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
               selectTab={() => handleTabChange("skills")}
               active={tab === "skills"}
             >
-              Skills
+              {" "}
+              Skills{" "}
             </TabButton>
             <TabButton
               selectTab={() => handleTabChange("education")}
               active={tab === "education"}
             >
-              Education
+              {" "}
+              Education{" "}
             </TabButton>
             <TabButton
               selectTab={() => handleTabChange("certifications")}
               active={tab === "certifications"}
             >
-              Certifications
+              {" "}
+              Certifications{" "}
             </TabButton>
           </div>
           <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab)?.content}
+            {TAB_DATA.find((t) => t.id === tab).content}
           </div>
         </div>
       </div>
