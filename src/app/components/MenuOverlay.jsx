35038ae0a0e2953e29d7679b0a8202f452b394c3
Eriko Syah <erikosyahputraf@gmail.com>
2025-01-15 18:21:30 +0700

    update: padding for menuoverlay

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -3,10 +3,10 @@ import NavLink from "./NavLink";
 
 const MenuOverlay = ({ links }) => {
   return (
-    <ul className="flex flex-col items-center">
+    <ul className="flex flex-col py-4 items-center">
       {links.map((link, index) => (
         <li key={index}>
-            <NavLink href={link.path} title={link.title} />
+          <NavLink href={link.path} title={link.title} />
         </li>
       ))}
     </ul>
